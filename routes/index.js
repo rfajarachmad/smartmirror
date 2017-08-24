c4fb35765e0ba86f0297728fc59eade21120bcbe
Haritian Pinio <haritianpinio@gmail.com>
2017-08-25 00:44:11 +0800

    changes :D

--- routes/index.js
+++ routes/index.js
@@ -3,26 +3,39 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index');
+  res.render('ra');
 });
 
-router.get('/w_and_t', function (req, res, next) {
-  res.render('weather-time', {});
+router.get('/ra', function (req, res, next) {
+  res.render('ra', {});
 });
 
-router.get('/stats', function (req, res, next) {
-  res.render('stats', {});
+router.get('/pt', function (req, res, next) {
+  res.render('pt', {});
+});
+
+router.get('/pl', function (req, res, next) {
+  res.render('pl', {});
+});
+
+router.get('/cdc', function (req, res, next) {
+  res.render('cdc', {});
 });
 
 router.get('/ann', function (req, res, next) {
   res.render('ann', {});
 });
 
-router.get('/voteus', function (req, res, next) {
-  res.render('voteus', {});
+router.get('/stats', function (req, res, next) {
+  res.render('stats', {});
 });
 
+router.get('/w_and_t', function (req, res, next) {
+  res.render('weather-time', {});
+});
 
-
+router.get('/hay', function (req, res, next) {
+  res.render('hay');
+});
 
 module.exports = router;
